7404d50647c765017b9a5f2ba4c8440bde14de70
Mel Colton <melcolton@icloud.com>
2023-06-26 11:37:01 +0100

    Main directory

---
+++ lib/services/api_keys.dart
@@ -0,0 +1,4 @@
+const currencyURL = 'https://openexchangerates.org/api/currencies.json?app_id=$key';
+const ratesURL = 'https://openexchangerates.org/api/latest.json?app_id=$key';
+
+const String key = '0ed807b6f8484df6a37cf48a7b870108';
